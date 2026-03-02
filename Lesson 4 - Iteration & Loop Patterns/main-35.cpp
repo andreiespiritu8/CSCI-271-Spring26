f4f07f50ee7a2e3d2ba25f67b5e988c34a713150
andreiespiritu8 <andrei.espiritu@jjay.cuny.edu>
2026-03-01 23:33:15 -0500

    Add files via upload

---
+++ Lesson 4 - Iteration & Loop Patterns/main-35.cpp
@@ -0,0 +1,39 @@
+/*
+Name: Andrei Espiritu
+Course: CSCI 271
+Lesson: Lesson 4 – Iteration & Loop Patterns
+Program: Counting from 1 to 10
+Description: This program counts from 1 to 10.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    for (unsigned int i = 1; i <= 10; ++i)
+    {
+        cout << i << " ";
+    }
+ return 0;
+} 
+   
+
+
+/*
+Reflection:
+1. What I understood from this program:
+This program uses counter-controlled iteration. The loop starts at 1 and ends at 10.
+
+2. Difficulties I faced:
+I faced no errors while reproducing this code example from the slides.
+
+3. What I needed to search or review:
+I needed to search the lecture slides and the examples. 
+
+4. AI Usage (if any):
+I used AI to help me understand the concepts.
+
+5. What I learned:
+I learned that we use unsigned int to prevent negative values.
+
+*/
